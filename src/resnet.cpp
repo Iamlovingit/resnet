d89068bc86c405748a88da1179283b2234c7f1d5
iamlovingit <bigxhome@163.com>
2019-10-23 02:18:50 -0700

    create resnet project

---
+++ src/resnet.cpp
@@ -0,0 +1,10 @@
+//
+// Created by bitfrog on 10/23/19.
+//
+
+#include "resnet.h"
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
---
+++ src/resnet.h
@@ -0,0 +1,7 @@
+//
+// Created by bitfrog on 10/23/19.
+//
+
+#pragma once;
+#include <iostream>
+#include <gflags/gflags.h>
